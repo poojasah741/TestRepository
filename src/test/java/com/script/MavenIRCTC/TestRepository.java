dd1e39721f84cccdfe3750a9ac9048dc8ac2a8d5
HP-N205 <HP-N205@HP>
2020-05-29 23:16:47 +0530

    changes done

---
+++ src/test/java/com/script/MavenIRCTC/TestRepository.java
@@ -0,0 +1,13 @@
+package com.script.MavenIRCTC;
+
+import org.testng.annotations.Test;
+
+public class TestRepository 
+{
+	@Test
+	public void verify()
+	{
+		System.out.println("Verify repository");
+	}
+
+}
